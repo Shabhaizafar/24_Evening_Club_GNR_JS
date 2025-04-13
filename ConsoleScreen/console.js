15c701be017f86b1b427d22d3b9499eb92b29532
Shabhaizafar <zafarshabhai03@gmail.com>
2025-04-13 15:29:57 +0530

    Init

---
+++ ConsoleScreen/console.js
@@ -0,0 +1,47 @@
+// document.write("Hello");
+// console.log("Hello");
+
+
+// How to Print Data/Display Data in a Console Screen: 
+
+// console.log("Hello Everyone");
+// console.log("Welcome to \nJavascript");
+// console.log("Welcome to \tJavascript");
+
+// console.log(12);
+// console.log(12.234);
+// console.log(true);
+
+// console.log([11,12,13,14]);
+
+
+// How to Print Data  a Table Format in a COnsole Screen : 
+// console.table([11,12,13,14]);
+
+
+// Showing Error : 
+// console.error("This is an Error!!");
+
+//Showing Warnning : 
+// console.warn("This is a Warnning!!");
+
+
+// Calculate Time : 
+// console.time("Time");
+// console.log("Hello");
+// console.log("Everyone");
+// console.log("Welcome");
+// console.timeEnd("Time");
+
+
+
+// Print Bio Data  : using log()
+
+// Print Your Subject Marks : using table()
+
+// Print your Goal :  using Error()
+
+// print royal  : using warn()
+
+
+// calculate time : 
