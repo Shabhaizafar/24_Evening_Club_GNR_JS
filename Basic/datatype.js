72a1ebbe0c6418fd1626c9494ff0c6cefed834aa
Shabhaizafar <zafarshabhai03@gmail.com>
2025-04-15 19:16:41 +0530

    Vriable,Datatype

---
+++ Basic/datatype.js
@@ -0,0 +1,77 @@
+// console.log("Hello ");
+
+
+// console.log("Zafar");
+// console.log(30);
+// console.log(3.14);
+
+
+// in C language : 
+// int ,float,double,char,struct 
+
+
+//Datatype in Javascript : 
+
+// number :-
+// string :-
+// Object :-
+// function :-
+// undefined :- 
+// BigInt :-
+// Boolean :- true/false
+
+
+// console.log(12);
+// console.log(typeof(12));
+
+// console.log(3.14);
+// console.log(typeof(3.14));
+
+// console.log('a');
+// console.log(typeof 'a');
+// console.log("a");
+// console.log(typeof "a");
+// console.log(`a`);
+// console.log(typeof `a`);
+
+// Double Quote :
+// console.log("This is a Multiline.");
+// Single Quote :
+// console.log('This is a Multiline.');
+// Back-tick : 
+// console.log(`This is a
+//      Multiline.`);
+
+
+// array  ,struct,..   : object
+
+// console.log([1,1,2,2,3,45]);
+// console.log(typeof [1,1,2,2,3,45]);
+
+// console.log({fname : "Raj"});
+// console.log(typeof {fname : "Raj"});
+
+// console.log(null);
+// console.log(typeof null);
+
+
+// console.log(undefined);
+// console.log(typeof undefined);
+
+// console.log(prompt);
+// console.log(typeof prompt);
+
+
+// void myfu();
+// printf();
+
+
+
+
+// Object-based ,Function-based ,class-based :
+
+
+// console.log(12345678901234567890n);
+// console.log(typeof 12345678901234567890n);
+
+
---
+++ Basic/variable.js
@@ -0,0 +1,56 @@
+// always unique : 
+// start with Number : No 
+// keyword : No 
+// special Char : No   (_,$)
+
+// variable include whitespace : No 
+
+
+// int float double char 
+
+
+
+// How to Declare a Variable : 
+// var,let,const : 
+
+// var num = 12;
+// var str = "Zafar";
+// console.log(num);
+// console.log(typeof num);
+
+// console.log(str);
+// console.log(typeof str);
+
+
+
+// let fname = "Raj";
+// console.log(fname);
+// console.log(typeof fname);
+
+
+// const lname = "Shah";
+// console.log(lname);
+// console.log(typeof lname);
+
+
+var a = 12;
+console.log("Value of A : ",a);
+// a = 100;
+// console.log("Value of A : ",a);
+var a = 1000;
+console.log("Value of A : ",a);
+
+
+let b = 13;
+console.log("Value of B : ",b);
+// b = 200;
+// console.log("Value of B : ",b);
+// let b = 3000;
+
+
+
+const c = 14;
+console.log("Value of C : ",c);
+// c = 300;
+// console.log("Value of C : ",c);
+// const c= 3;
