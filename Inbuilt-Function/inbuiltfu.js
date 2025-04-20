39fd3dc09f8391094fae68bb548cb027c08e0ebd
Shabhaizafar <zafarshabhai03@gmail.com>
2025-04-20 15:16:06 +0530

    All

---
+++ Inbuilt-Function/inbuiltfu.js
@@ -0,0 +1,72 @@
+// console.log("Hello ");
+//  Function :
+//- Block of Code.
+
+// There are divided in a 2 Types : 
+// 1. in-Built Function (pre-defined) : 
+// Basic : 
+    // 1. prompt() : User input
+
+    // return   :string/null
+// var num = prompt("Enter your Age : ");
+// console.log("Your Age is : ",num);
+
+// var num1 = prompt("Enter Num1 : ");
+// var num2 = prompt("Enter Num2 : ");
+// console.log("Num 1 : ",num1,typeof num1);
+// console.log("Num 2 : ",num2,typeof num2);
+// console.log("Addition : ",num1+num2);
+
+
+// Number : 
+// Method 1 
+// var num1 = +prompt("Enter Num1 : ");
+// var num2 = +prompt("Enter Num2 : ");
+// console.log("Num 1 : ",num1,typeof num1);
+// console.log("Num 2 : ",num2,typeof num2);
+// console.log("Addition : ",num1+num2);
+
+// var num1 = Number(prompt("Enter Num1 : "));
+// var num2 = Number(prompt("Enter Num2 : "));
+// console.log("Num 1 : ",num1,typeof num1);
+// console.log("Num 2 : ",num2,typeof num2);
+// console.log("Addition : ",num1+num2);
+
+
+// Boolean : 
+// var a = Boolean(prompt());
+// console.log(a,typeof a);
+
+    // 2. confirm() :
+
+// var ans = confirm("Are you Ready ? ");
+// console.log(ans);
+
+    // 3. alert() : 
+
+// var ans = alert("Have a Good Day !!");
+// console.log(ans);
+
+
+// console.log(parseInt(1234.234));
+
+
+// 2. User Defined
+
+// (function(){
+//     console.log("Hello Everyone");
+// })();
+
+
+
+// (function(){
+//     console.log("Royal");
+// })();
+
+
+
+// === !==
+
+// console.log(12==12);  // true
+// console.log(12=="12");// true
+// console.log(12!=="12");// false
---
+++ Operators/op1.js
@@ -0,0 +1,105 @@
+//  Operators : 
+
+// Arithmatic+unary :  +,-,*,/,% , ++,--
+// Relational :  <,>,<=,>=,==,!=,===,!==
+// Logical :  && ,|| , !
+// Assignment :  =, += /=,*=,%=
+// Ternary  : (condition)? st1 : st2;
+// Type of Op : typeof()
+
+
+
+// var a = 12;
+// var b = 13;
+// console.log(a+b);//25
+
+// var a = 12;
+// var b = "13";
+// console.log(a+b);//1213
+
+// var a = 12;
+// var b = "13";
+// var c = 2;
+// console.log(a+c+b);//1413
+// console.log(a+b+c);// 12132
+
+// var a = 12;
+// var b = "Zafar";
+// console.log(a+b); //12Zafar
+
+// var a = 12;
+// var b = "13";
+// console.log(a-b);//-1
+
+
+// var a = 12;
+// var b = "10";
+// console.log(a-b);
+
+
+
+
+// var a = 12;
+// var b = "12";
+// console.log(a==b); //true
+// console.log(a!=b); //false
+
+// console.log(a===b);//false
+// console.log(a!==b);//true
+
+
+// console.log(12<13 && 13<50); //true
+// console.log(12>13 && 13<50); //false
+        // ""
+// console.log(12<13 && 13); // 13
+// console.log(13 && 12<13); // true
+
+// console.log(12!=13 && "Zafar"); //Zafar
+
+// console.log(null && 10000); //10000
+
+
+
+// and : 
+
+// 0   0   0
+// 0   1   0
+// 1   0   0
+// 1   1   1
+
+
+// or : 
+// 0   0   0   
+// 0   1   1
+// 1   0   1
+// 1   1   1
+
+// console.log(12<13 || 13<50);//true
+// console.log(12>13 || 13<50);//true
+// console.log(12<13 || 13>50);//true
+
+// console.log(12<13 || 13); //true 
+// console.log(12>13 || 13); //13
+
+
+// console.log(12>13 || 13 || "Zafar"); 
+
+// var age = 1;
+// var product = (age>=5) ? "Tea" : "Milk";
+// console.log(product);
+
+
+
+
+
+
+// let name = prompt("Enter Your Name : ");
+// alert("Hello,"+name);
+
+
+
+// console.log("12"<13);
+
+// console.log("22"<"133");
+
+console.log("12">="");
