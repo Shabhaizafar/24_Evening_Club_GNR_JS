997b55f1ec51f6df4e26525b1a8f0e9ba73902aa
Shabhaizafar <zafarshabhai03@gmail.com>
2025-04-21 09:01:39 +0530

    Backup Completed

---
+++ Backup/external.js
@@ -0,0 +1,22 @@
+// How to Print/Display Data in a Main Screen :-
+document.writeln("Hello Everyone");
+
+// How To display/Print Data in a Console Screen :-
+console.time("Time");
+console.log("Hello");
+console.log(12);
+console.log(true);
+console.log([11,1,2,2]);
+
+
+// How to Display Data in a Table Format :- 
+console.table([11,1,2,2]);
+
+
+// How to display Error msg : 
+console.error("This is an Error !!!");
+
+// Show warning : 
+console.warn("Warning");
+console.timeEnd("Time");
+
