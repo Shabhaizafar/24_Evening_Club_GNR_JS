1f6081eb05aa253822811b93b7bded4711957a19
Shabhaizafar <zafarshabhai03@gmail.com>
2025-05-13 14:53:24 +0530

    Array

---
+++ Backup/Array.js
@@ -0,0 +1,87 @@
+// What is an Array ? 
+// It's a Collection of Values which is Same or different Datatype.
+
+
+// const arr1 = [11,12,13];
+// console.log(arr1);
+
+// const arr2 = ["Royal","Techno"];
+// console.log(arr2);
+
+// const arr3 = [true,false];
+// console.log(arr3);
+
+// const arr4  = [12,"Royal",true,null,undefined];
+// console.log(arr4);
+
+
+// 
+// var arr = [11,12,13,14,15,16,17,500];
+// index   0   1  2  3 4 ....
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[2]);
+// console.log(arr[5]);
+// console.log(arr[arr.length-1]);
+
+// property  : 
+// length : 
+// console.log(arr.length);
+
+
+// var arr = [11,12,13,14,15,16,17];
+// console.log(arr);
+
+// Methods :
+
+// 1. push : add a new value from last index
+// return : new length of Array
+// single Value
+// var ans = arr.push(200);
+// Multiple Value
+// arr.push(100,200,300);
+
+
+
+// 2. pop : remove value fro last index
+//  return : deleted/removed value
+// var ans = arr.pop(); // 11-16
+// arr.pop();// 11-15
+
+
+
+// 3. ushift : add a new value from 0 index
+// return : new length of Array
+// Single Value
+// arr.unshift(100);
+// Multiple Value
+// arr.unshift(100,500);
+
+// 4. shift : remove value from 0 index
+//  return : deleted/removed value
+// arr.shift();
+// arr.shift();
+
+// console.log(arr);
+// console.log(ans);
+
+
+// var arr = [11,12,13,14,15,16,17];
+// console.log(arr);
+// delete arr[3];
+// console.log(arr);
+
+
+// var  : 
+// var a=12;
+// a=13
+// var a=13;
+
+// let  :
+// let b = 14;
+// let b=15;
+
+// const: 
+// const c = 12;
+// c=13;
+// const c = 17;
