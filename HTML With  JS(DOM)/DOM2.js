c2e1a00e13c6d90f89fabb952e9e9c207249f1eb
Shabhaizafar <zafarshabhai03@gmail.com>
2025-05-19 19:49:51 +0530

    DOM2

---
+++ HTML With  JS(DOM)/DOM2.js
@@ -0,0 +1,20 @@
+// How to Access HTML Content using JS:
+
+
+// 1. using innerHTML
+// console.log(document.querySelector('div').innerHTML);
+
+// 2. Using innerText
+// console.log(document.querySelector('div').innerText);
+
+// 3. using textContent : 
+// console.log(document.querySelector('div').textContent);
+
+
+
+
+// How to Change/Modify HTML Element Content Using JS : 
+
+// document.querySelector('h1').innerHTML = "Niya Patel";
+// document.querySelector('h2').innerText = "Hetavi Makwana";
+// document.querySelector('h3').textContent = "Prachi Nayak";
