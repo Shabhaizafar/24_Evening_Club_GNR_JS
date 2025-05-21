fd7300689a2d5d1eb31c4bf5f242f21b139b1c5b
Shabhaizafar <zafarshabhai03@gmail.com>
2025-05-21 15:51:49 +0530

    Slider

---
+++ HTML With  JS(DOM)/slider.js
@@ -0,0 +1,24 @@
+var btn_left = document.querySelectorAll('button')[0];
+var btn_right = document.querySelectorAll('button')[1];
+
+var counter = 0;
+
+btn_right.onclick = function () {
+    counter++;
+    if (counter == 1) {
+        document.querySelectorAll('div')[0].style.left = "-100%";
+        document.querySelectorAll('div')[1].style.left = "0%";
+        document.querySelectorAll('div')[2].style.left = "100%";
+    }
+    else if (counter == 2) {
+        document.querySelectorAll('div')[0].style.left = "-200%";
+        document.querySelectorAll('div')[1].style.left = "-100%";
+        document.querySelectorAll('div')[2].style.left = "0%";
+    }
+    else if (counter == 3) {
+        counter = 0;
+        document.querySelectorAll('div')[0].style.left = "0%";
+        document.querySelectorAll('div')[1].style.left = "100%";
+        document.querySelectorAll('div')[2].style.left = "200%";
+    }
+}
